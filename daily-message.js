dd7e8fa9ff1b47f88abec1568d19ccf50dd22c44
Denver <denver.clark96@gmail.com>
2021-09-22 11:24:10 -0500

    correct file issues

--- daily-message.js
+++ daily-message.js
@@ -1,10 +1,3 @@
-//Message Options: Array of random inspirational and motivational quotes:
-let quotes = ['The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela',
-"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
-"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
-"The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt"];
-let luckyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const randomMessage = () => {
     //Message Options: Array of random inspirational and motivational quotes:
     let quotes = ['The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela', "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs", "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron", "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt"];
